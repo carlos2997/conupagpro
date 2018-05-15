a7e4c22f540c7bfa4dc2d306434df53d933ce752
carlos2997 <carlosramirez-2997@hotmail.com>
2018-05-14 22:42:25 -0500

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+sudo git clone https://github.com/carlos2997/conupagpro
+cd conupagpro
+sudo npm init -y
+sudo npm install tedious
+sudo npm install readline-sync
+sudo npm install async
+sudo npm install express -y
+node page.js
