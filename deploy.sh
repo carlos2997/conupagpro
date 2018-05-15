3059d120c085bd115494aadfb8003241ad6b7a5a
carlos2997 <carlosramirez-2997@hotmail.com>
2018-05-14 22:51:15 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 sudo git clone https://github.com/carlos2997/conupagpro
 cd conupagpro
+sudo apt-get install nodejs
+sudo apt-get install npm
 sudo npm init -y
 sudo npm install tedious
 sudo npm install readline-sync
